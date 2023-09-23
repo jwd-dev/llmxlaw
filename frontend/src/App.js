e3c50ab385a7f378ae394a1df8fa0617f1b35a7c
Jake Downie <jake@jdocs.app>
2023-09-23 16:34:46 -0700

    final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,29 +3,21 @@ import { Chrono } from "react-chrono";
 
 const App = () => {
 
-  const items = [{
-    title: "May 1940",
-    cardTitle: "Dunkirk",
-    url: "http://www.history.com",
-    cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-    cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-  },
-    {
-      title: "May 1940",
-      cardTitle: "Dunkirk",
-      url: "http://www.history.com",
-      cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+  const transformData = (data) => {
+    return data.map(entry => ({
+      title: entry[2], // Assuming this is the date you want as the title
+      cardTitle: entry[0],
+      url: "", // Placeholder as the original data does not provide a URL
+      cardSubtitle: entry[1],
+      cardDetailedText: entry[1]
+    }));
+  };
+  const inputData = [
+    ["Imposition of Sentence on Paras Jha","The court hearing for the imposition of sentence on Paras Jha took place","09-18-2018 1:08 PM","09-18-2018 1:08 PM","d313286829947beeb9df0b0e30ec2be6"],
+    ["Imposition of Sentence on Paras Jha","The court hearing for the imposition of sentence on Paras Jha took place.","09-18-2018","09-18-2018","d313286829947beeb9df0b0e30ec2be6"]
+  ];
 
-    },
-    {
-      title: "May 1940",
-      cardTitle: "Dunkirk",
-      url: "http://www.history.com",
-      cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-
-    }];
+  const items = transformData(inputData);
 
   return (
       <div style={{ width: '500px', height: '950px' }}>
