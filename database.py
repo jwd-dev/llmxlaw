61dd5f42027ce05452938d00efaddd0925875cbb
Bailey <baileyg2016@gmail.com>
2023-09-23 15:30:05 -0700

    updating with a start and endtime

--- database.py
+++ database.py
@@ -9,20 +9,23 @@ class Database:
       CREATE TABLE "Events" (
         "eventName" text,
         "description" text,
-        "datetime" text,
+        "start_time" datetime,
+        "end_time" datetime,
         "referencedDocuments" text
       )
     """)
 
 
   def add_event(self, eventName, description, datetime, referenced_documents=""):
-    c.execute("INSERT INTO 'Events' ('eventName', 'description', 'datetime', 'referencedDocuments') VALUES (?, ?, ?, ?)",
+    c.execute("INSERT INTO 'Events' ('eventName', 'description', 'start_time', 'end_time', 'referencedDocuments') VALUES (?, ?, ?, ?, ?)",
               (eventName, description, datetime, referenced_documents))
     conn.commit()
 
   def query_events(self):
     c.execute("SELECT * FROM 'Events'")
     return c.fetchall()
+  
+
 
 # add_event("Imposition of Sentence on Paras Jha", "The court hearing for the imposition of sentence on Paras Jha took place", "09-18-2018 09-18-2018")
 # print(query_events())
